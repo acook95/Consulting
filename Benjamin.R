44e2416601727dedf0b5904dd0b92da127b11748
Anna Cook <annacook@bu.edu>
2021-02-26 08:25:49 -0500

    added 50% effect size

--- Benjamin.R
+++ Benjamin.R
@@ -1,35 +1,26 @@
 library(pwr)
 library(pwr2)
 
-pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.05, type = "paired", alternative = "greater")
-
-samplesizes <- seq(from=40,to=150,by=10)
-power.samplesizes <- power.t.test(n=samplesizes,d = 0.3, sig.level = 0.05, type = "paired")$power
-plot(samplesizes,
-     power.samplesizes,
-     xlim=c(40,150),
-     xlab="Sample size",
-     ylab="Expected power",
-     ylim=c(0,1),
-     type="b",
-     col="darkorange",
-     lwd=5,axes=FALSE)
-axis(1,at=c(0,50,100,150,200))
-axis(2,at=c(0,0.25,0.5,0.75,1),labels=paste(c(0,25,50,75,100),"%"))
-
-
-power1 <- pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.05, type = "two.sample", alternative = "greater")
-
+# alpha = 0.05, effect size = 30%
+power1 <- pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.05,
+                     type = "two.sample", alternative = "greater")
 plot(power1)
 
-power2 <- pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.01, type = "two.sample", alternative = "greater")
 
+# alpha = 0.01, effect size = 30%
+power2 <- pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.01,
+                     type = "two.sample", alternative = "greater")
 plot(power2)
 
-power2
 
-power3 <- pwr.t.test(n=NULL, power = 0.8, d = 0.3, sig.level = 0.05, type = "two.sample", alternative = "greater")
-
+# alpha = 0.05, effect size = 50%
+power3 <- pwr.t.test(n=NULL, power = 0.8, d = 0.5, sig.level = 0.05,
+                     type = "two.sample", alternative = "greater")
 plot(power3)
 
+
+# alpha = 0.01, effect size = 50%
+power4 <- pwr.t.test(n=NULL, power = 0.8, d = 0.5, sig.level = 0.01,
+                     type = "two.sample", alternative = "greater")
+plot(power4)
 
